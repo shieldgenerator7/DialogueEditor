c658e631e753f809e406e5cc24b3ceb2208feaca
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-09-21 03:25:17 -0400

    Add data classes

---
+++ src/DialoguePath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogueEditor.src
+{
+    public class DialoguePath
+    {
+
+        public List<Quote> quotes;
+    }
+}
---
+++ src/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Quote
+{
+
+	public string characterName;
+	public string text;
+
+	public Quote(string charName="TEST1", string txt="Text test 2.")
+	{
+		this.characterName = charName;
+		this.text = txt;
+	}
+}
