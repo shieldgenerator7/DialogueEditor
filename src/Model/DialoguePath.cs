02d7358d3a3102f50a00e85caef7746bcd22e512
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-09-23 21:12:03 -0400

    Modify model classes for Unity

--- src/Model/DialoguePath.cs
+++ src/Model/DialoguePath.cs
@@ -1,24 +1,20 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DialogueEditor.src
+[Serializable]
+public class DialoguePath
 {
-    public class DialoguePath
-    {
-        public string title = "Dialogue Title";
-        public List<Quote> quotes = new List<Quote>();
+    public string title = "Dialogue Title";
+    public List<Quote> quotes = new List<Quote>();
 
-        /// <summary>
-        /// Restores temp variables after being read in
-        /// </summary>
-        public void inflate()
-        {
-            quotes.ForEach(
-                q => q.path = this
-                );
-        }
+    /// <summary>
+    /// Restores temp variables after being read in
+    /// </summary>
+    public void inflate()
+    {
+        quotes.ForEach(
+            q => q.path = this
+            );
     }
 }
--- src/Model/Quote.cs
+++ src/Model/Quote.cs
@@ -1,22 +1,22 @@
-﻿using DialogueEditor.src;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 
+[Serializable]
 public class Quote
 {
 
-	public string characterName;
-	public string text;
+    public string characterName;
+    public string text;
 
-	[NonSerialized]
-	public DialoguePath path;
+    [NonSerialized]
+    public DialoguePath path;
 
-	[JsonIgnore]
-	public int Index => path.quotes.IndexOf(this);
+    [JsonIgnore]
+    public int Index => path.quotes.IndexOf(this);
 
-	public Quote(string charName="", string txt="")
-	{
-		this.characterName = charName;
-		this.text = txt;
-	}
+    public Quote(string charName = "", string txt = "")
+    {
+        this.characterName = charName;
+        this.text = txt;
+    }
 }
