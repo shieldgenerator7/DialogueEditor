b2120f939b33418e2fcd49ef3d5ca4f45dd07b5b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-09-26 16:41:16 -0400

    Add Quote.imageName

--- src/Model/Quote.cs
+++ src/Model/Quote.cs
@@ -7,6 +7,10 @@ public class Quote
 
     public string characterName = "";
     public string text = "";
+    /// <summary>
+    /// The image filename without the folder path and without the file extension
+    /// </summary>
+    public string imageName = "";
     public string imageFileName = "";
 
     [NonSerialized]
@@ -20,5 +24,10 @@ public class Quote
         this.characterName = charName;
         this.text = txt;
         this.imageFileName = imageFileName;
+        //set image name
+        string[] split = imageFileName.Split(new char[] { '\\', '/' });
+        string name = split[split.Length - 1];
+        int lastDotIndex = name.LastIndexOf('.');
+        this.imageName = name.Substring(0, lastDotIndex);
     }
 }
