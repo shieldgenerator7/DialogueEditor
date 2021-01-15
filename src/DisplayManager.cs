20754b294b84987bdba9d12245c24c454e555062
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-15 18:59:29 -0500

    Extract layout code from DisplayManager

--- src/DisplayManager.cs
+++ src/DisplayManager.cs
@@ -6,13 +6,8 @@ using System.Linq;
 
 public class DisplayManager
 {
-    const int MAX_WIDTH = 250;
-    const int BUFFER_WIDTH = 10;
-
-    /// <summary>
-    /// Where the next Node will be placed
-    /// </summary>
-    Vector cursor = new Vector(BUFFER_WIDTH, BUFFER_WIDTH);
+    public const int MAX_WIDTH = 250;
+    public const int BUFFER_WIDTH = 10;
 
     const int fontSize = 15;
     Font font = new Font("Ariel", fontSize);
@@ -21,7 +16,7 @@ public class DisplayManager
 
     Brush backBrush = new SolidBrush(Color.LightGray);
 
-    const int portraitSize = 50;
+    public const int portraitSize = 50;
 
     Graphics g;
 
@@ -37,9 +32,9 @@ public class DisplayManager
     public void paint(Graphics g, Panel panel)
     {
         this.g = g;
+        Managers.Layout.layoutNodes();
         Vector mapPos = Vector.zero;
         Vector panelSize = new Vector(panel.Size);
-        cursor = new Vector(BUFFER_WIDTH, BUFFER_WIDTH);
         Managers.Node.containers
             .FindAll(n => nodeOnScreen(n, mapPos, panelSize))
             .ForEach(n => paintNode(n));
@@ -73,30 +68,13 @@ public class DisplayManager
     }
     private void paintNode(NodeDialogue nd)
     {
-        nd.position = cursor;
-        cursor.x += BUFFER_WIDTH;
-        cursor.y += BUFFER_WIDTH;
-        nd.size = Vector.zero;
-        //Set size
-        nd.Nodes.ForEach(n =>
-        {
-            sizeNode(n);
-            nd.size.x = Math.Max(nd.size.x, n.size.x);
-            nd.size.y += n.size.y + BUFFER_WIDTH;
-        });
-        nd.size.x += BUFFER_WIDTH * 2;
-        nd.size.y -= BUFFER_WIDTH;
         //Draw back
         g.FillRectangle(backBrush, nd.position.x, nd.position.y, nd.size.x, nd.size.y);
         //Draw nodes
         nd.Nodes.ForEach(n => paintNode(n));
-        //Update cursor
-        cursor.x += nd.size.x;
-        cursor.y = BUFFER_WIDTH;
     }
     private void paintNode(NodeQuote nq)
     {
-        nq.position = cursor;
         string text = nq.QuoteText;
         Vector textPosition = nq.position + Vector.right * (portraitSize + BUFFER_WIDTH);
         int imageWidth = portraitSize + BUFFER_WIDTH;
@@ -110,24 +88,6 @@ public class DisplayManager
         {
             g.DrawImage(nq.image, nq.position.x, nq.position.y, portraitSize, portraitSize);
         }
-        cursor.y += nq.size.y + BUFFER_WIDTH;
-    }
-
-    private void sizeNode(Node n)
-    {
-        if (n is NodeQuote)
-        {
-            sizeNode((NodeQuote)n);
-        }
-    }
-    private void sizeNode(NodeQuote nq)
-    {
-        string text = nq.QuoteText;
-        int imageWidth = portraitSize + BUFFER_WIDTH;
-        int textWidth = MAX_WIDTH - imageWidth;
-        nq.size = measureString(text, textWidth);
-        nq.size.x += imageWidth;
-        nq.size.y = Math.Max(nq.size.y, portraitSize);
     }
     #endregion
 
--- src/LayoutManager.cs
+++ src/LayoutManager.cs
@@ -1,8 +1,59 @@
-﻿using System;
+﻿using DialogueEditor.src;
+using System;
 
 public class LayoutManager
 {
-	public LayoutManager()
-	{
-	}
+
+    /// <summary>
+    /// Where the next Node will be placed
+    /// </summary>
+    Vector cursor = new Vector(DisplayManager.BUFFER_WIDTH, DisplayManager.BUFFER_WIDTH);
+
+    public void layoutNodes()
+    {
+        cursor = new Vector(DisplayManager.BUFFER_WIDTH, DisplayManager.BUFFER_WIDTH);
+        Managers.Node.containers.ForEach(c => layoutNode(c));
+    }
+
+    private void layoutNode(Node n)
+    {
+        if (n is NodeDialogue)
+        {
+            layoutNode((NodeDialogue)n);
+        }
+        if (n is NodeQuote)
+        {
+            layoutNode((NodeQuote)n);
+        }
+    }
+    private void layoutNode(NodeDialogue nd)
+    {
+        nd.position = cursor;
+        cursor.x += DisplayManager.BUFFER_WIDTH;
+        cursor.y += DisplayManager.BUFFER_WIDTH;
+        nd.size = Vector.zero;
+        //Set size
+        nd.Nodes.ForEach(n =>
+        {
+            layoutNode(n);
+            nd.size.x = Math.Max(nd.size.x, n.size.x);
+            nd.size.y += n.size.y + DisplayManager.BUFFER_WIDTH;
+        });
+        nd.size.x += DisplayManager.BUFFER_WIDTH * 2;
+        nd.size.y -= DisplayManager.BUFFER_WIDTH;
+        //Update cursor
+        cursor.x += nd.size.x;
+        cursor.y = DisplayManager.BUFFER_WIDTH;
+    }
+    private void layoutNode(NodeQuote nq)
+    {
+        nq.position = cursor;
+        string text = nq.QuoteText;
+        int imageWidth = DisplayManager.portraitSize + DisplayManager.BUFFER_WIDTH;
+        int textWidth = DisplayManager.MAX_WIDTH - imageWidth;
+        nq.size = Managers.Display.measureString(text, textWidth);
+        nq.size.x += imageWidth;
+        nq.size.y = Math.Max(nq.size.y, DisplayManager.portraitSize);
+        cursor.y += nq.size.y + DisplayManager.BUFFER_WIDTH;
+    }
 }
