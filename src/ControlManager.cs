b29903172caaee473a0f91049a6dd46a198a6dd2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-20 19:58:37 -0500

    Extract SelectionManager from ControlManager

--- src/ControlManager.cs
+++ src/ControlManager.cs
@@ -8,7 +8,6 @@ using System.Windows.Forms;
 
 public class ControlManager
 {
-    private List<Node> selectedNodes = new List<Node>();
     public ControlManager()
     {
     }
@@ -21,57 +20,9 @@ public class ControlManager
     public void select(Node node)
     {
         //2020-09-27: copied from https://stackoverflow.com/a/973733/2336212
-        select(node, Control.ModifierKeys == Keys.Shift);
+        Managers.Select.select(node, Control.ModifierKeys == Keys.Shift);
     }
 
-    /// <summary>
-    /// Selects the given NodeDialogue or Node subtype
-    /// </summary>
-    /// <param name="node"></param>
-    /// <param name="append">true to add to list, false to overwrite it</param>
-    public void select(Node node, bool append)
-    {
-        if (node is NodeDialogue || node is Node)
-        {
-            if (!append)
-            {
-                //selectedNodes.ForEach(c => c.BackColor = Managers.Colors.unselectColor);
-                selectedNodes.Clear();
-            }
-            if (!selectedNodes.Contains(node))
-            {
-                //node.BackColor = Managers.Colors.selectColor;
-                selectedNodes.Add(node);
-            }
-        }
-    }
-
-    public void deselect(Node node)
-    {
-        //node.BackColor = Managers.Colors.unselectColor;
-        selectedNodes.Remove(node);
-        if (node is NodeQuote)
-        {
-            ((NodeQuote)node).Editing = false;
-        }
-    }
-
-    /// <summary>
-    /// Deselect everything
-    /// </summary>
-    public void deselectAll()
-    {
-        for (int i = selectedNodes.Count - 1; i >= 0; i--)
-        {
-            deselect(selectedNodes[i]);
-        }
-    }
-
-    public void processSelectedNodes(Action<Node> action)
-    {
-        selectedNodes.ForEach(n => action(n));
-    }
-        
     public DialoguePath createDialoguePath()
     {
         //Create a new quote with no path,
@@ -84,9 +35,7 @@ public class ControlManager
 
     public void createQuote()
     {
-        List<Node> prevSelected = new List<Node>(selectedNodes);
-        deselectAll();
-        prevSelected.ForEach(
+        bool anyPrevSelected = Managers.Select.processPrevSelectedNodes(
             n =>
             {
                 DialoguePath path = n.Path;
@@ -95,10 +44,9 @@ public class ControlManager
                     : -1;
                 NodeQuote node = Managers.Node.createNodeQuote(path, index);
                 node.Editing = true;
-                select(node, true);
-            }
-            );
-        if (prevSelected.Count == 0)
+                Managers.Select.select(node, true);
+            });
+        if (!anyPrevSelected)
         {
             //If nothing is selected, make a new dialogue path
             //(which will auto-create a new quote too)
@@ -108,17 +56,15 @@ public class ControlManager
 
     public void createCondition()
     {
-        List<Node> prevSelected = new List<Node>(selectedNodes);
-        deselectAll();
-        prevSelected.ForEach(
+        bool anyPrevSelected = Managers.Select.processPrevSelectedNodes(
             n =>
             {
                 DialoguePath path = n.Path;
                 NodeCondition node = Managers.Node.createNodeCondition(path);
-                select(node, true);
+                Managers.Select.select(node, true);
             }
             );
-        if (prevSelected.Count == 0)
+        if (!anyPrevSelected)
         {
             //If nothing is selected, make a new dialogue path
             //(which will auto-create a new quote too)
@@ -129,17 +75,15 @@ public class ControlManager
 
     public void createAction()
     {
-        List<Node> prevSelected = new List<Node>(selectedNodes);
-        deselectAll();
-        prevSelected.ForEach(
+        bool anyPrevSelected = Managers.Select.processPrevSelectedNodes(
             n =>
             {
                 DialoguePath path = n.Path;
                 NodeAction node = Managers.Node.createNodeAction(path);
-                select(node, true);
+                Managers.Select.select(node, true);
             }
             );
-        if (prevSelected.Count == 0)
+        if (!anyPrevSelected)
         {
             //If nothing is selected, make a new dialogue path
             //(which will auto-create a new quote too)
@@ -150,9 +94,7 @@ public class ControlManager
 
     public void enterPressed()
     {
-        NodeQuote activeNode = (NodeQuote)selectedNodes.FirstOrDefault(
-            c => c is NodeQuote
-            );
+        NodeQuote activeNode = Managers.Select.getOne<NodeQuote>();
         if (activeNode != null)
         {
             activeNode.Editing = false;
@@ -170,7 +112,7 @@ public class ControlManager
 
     public void escapePressed()
     {
-        deselectAll();
+        Managers.Select.deselectAll();
     }
 
     /// <summary>
@@ -181,10 +123,10 @@ public class ControlManager
     {
 
         //Don't delete anything if a selected node is being edited
-        if (!selectedNodes.Any(c => c is NodeQuote && ((NodeQuote)c).Editing))
+        if (!Managers.Select.EditNode)
         {
             bool canDeleteAll = true;
-            List<Node> dialogues = selectedNodes.FindAll(n => n is NodeDialogue);
+            List<NodeDialogue> dialogues = Managers.Select.getAll<NodeDialogue>();
             if (dialogues.Count > 0)
             {
                 string titles = "";
@@ -204,8 +146,7 @@ public class ControlManager
             }
             if (canDeleteAll)
             {
-                selectedNodes.ForEach(n => Managers.Node.delete(n));
-                selectedNodes.Clear();
+                Managers.Select.processPrevSelectedNodes(n => Managers.Node.delete(n));
                 return true;
             }
         }
--- src/SelectionManager.cs
+++ src/SelectionManager.cs
@@ -1,8 +1,85 @@
-﻿using System;
+﻿using DialogueEditor.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SelectionManager
 {
-	public SelectionManager()
-	{
-	}
+    private List<Node> selectedNodes = new List<Node>();
+    private Node editingNode = null;//the node that's currently being edited
+    public Node EditNode
+    {
+        get => editingNode;
+        set => editingNode = value;
+    }
+
+    public SelectionManager()
+    {
+    }
+
+    /// <summary>
+    /// Selects the given NodeDialogue or Node subtype
+    /// </summary>
+    /// <param name="node"></param>
+    /// <param name="append">true to add to list, false to overwrite it</param>
+    public void select(Node node, bool append)
+    {
+        if (node is NodeDialogue || node is Node)
+        {
+            if (!append)
+            {
+                //selectedNodes.ForEach(c => c.BackColor = Managers.Colors.unselectColor);
+                selectedNodes.Clear();
+            }
+            if (!selectedNodes.Contains(node))
+            {
+                //node.BackColor = Managers.Colors.selectColor;
+                selectedNodes.Add(node);
+            }
+        }
+    }
+    public void deselect(Node node)
+    {
+        //node.BackColor = Managers.Colors.unselectColor;
+        selectedNodes.Remove(node);
+        if (node is NodeQuote)
+        {
+            ((NodeQuote)node).Editing = false;
+        }
+    }
+
+    /// <summary>
+    /// Deselect everything
+    /// </summary>
+    public void deselectAll()
+    {
+        for (int i = selectedNodes.Count - 1; i >= 0; i--)
+        {
+            deselect(selectedNodes[i]);
+        }
+    }
+
+    public T getOne<T>() where T : Node
+        => (T)selectedNodes.FirstOrDefault(n => n is T);
+
+    public List<T> getAll<T>() where T : Node
+       => selectedNodes.FindAll(n => n is T).ConvertAll(n => (T)n);
+
+    public void processSelectedNodes(Action<Node> action)
+    {
+        selectedNodes.ForEach(n => action(n));
+    }
+
+    /// <summary>
+    /// Deselects all nodes, then processes the now-previously selected nodes.
+    /// </summary>
+    /// <param name="action"></param>
+    /// <returns>True: there was at least 1 previously selected node</returns>
+    public bool processPrevSelectedNodes(Action<Node> action)
+    {
+        List<Node> prevSelected = new List<Node>(selectedNodes);
+        deselectAll();
+        selectedNodes.ForEach(n => action(n));
+        return prevSelected.Count > 0;
+    }
 }
