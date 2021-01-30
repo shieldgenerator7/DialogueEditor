41e12f63c641592ef15bc324e0767b39e76ca292
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-30 01:57:25 -0500

    Add voice line icons to resources

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace DialogueEditor.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap noVoiceLine {
+            get {
+                object obj = ResourceManager.GetObject("noVoiceLine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap setVoiceLine {
+            get {
+                object obj = ResourceManager.GetObject("setVoiceLine", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
